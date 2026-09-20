e2c35b9ffeeab81ff64170302b08b43ff34c7126
Anthony Ching <57060513+EnergeticTony@users.noreply.github.com>
2026-09-19 22:31:59 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
