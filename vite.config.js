8116e0271df6b4e02dbb5d057d0251f0150a52b1
Anthony Ching <57060513+EnergeticTony@users.noreply.github.com>
2026-09-19 22:18:26 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // This site is deployed at the root of lunas-thrift-store.github.io.
-  base: '/',
+  base: '/lunas-thrift-store.github.io/',
 })
